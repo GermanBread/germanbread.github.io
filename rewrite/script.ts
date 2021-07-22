62af048aba947a44a87c7e072e2d53f28a238c79
GermanBread <nd01@online.de>
2021-07-22 14:58:31 +0200

    rewrite?

---
+++ rewrite/script.ts
@@ -0,0 +1,15 @@
+var nav : HTMLElement
+function init() {
+    nav = document.getElementById("navmenu")
+    handlescroll()
+
+    window.addEventListener("scroll", handlescroll)
+}
+
+function handlescroll() {
+    if (window.scrollY > 50) {
+        nav.classList.add("attached")
+    } else {
+        nav.classList.remove("attached")
+    }
+}
