47658cab3eacd2a9563b1b98d51f79f8abf206b3
GermanBread <nd01@online.de>
2021-08-13 17:40:18 +0200

    I'm done for today, still need to fix some issues for smaller resolutions

--- rewrite/script.ts
+++ rewrite/script.ts
@@ -3,14 +3,16 @@ var about : HTMLElement, about_content : HTMLElement,
 var about_rect : DOMRect,
     repos_rect : DOMRect
 
-var transition_offset : number = 40 * (window.innerHeight / 100)
+var transition_offset : number = 40 * (window.innerHeight / 100) * (window.innerWidth / 2500)
 var transition_smoothness : number = 40 * (window.innerHeight / 100);
 var parallax_multiplier : number = 1 * (window.innerHeight / 100);
 
 var values_initialised : boolean = false
 
 function init() {
-    
+    fetchpersonalrepos(document.getElementById("repos-list"))
+    fetchcontibutionrepos(document.getElementById("contributions-list"))
+
     if (!values_initialised) {
         about = document.getElementById("section-about")
         about_content = document.getElementById("section-about-content")
@@ -29,30 +31,29 @@ function init() {
 
     document.getElementById("repos-list").innerHTML = ""
     document.getElementById("contributions-list").innerHTML = ""
-
-    fetchpersonalrepos(document.getElementById("repos-list"))
-    fetchcontibutionrepos(document.getElementById("contributions-list"))
 }
 
 // Copied from https://stackoverflow.com/questions/11409895/whats-the-most-elegant-way-to-cap-a-number-to-a-segment
-function clamp(num : number, min :number, max :number) : number {
-    return num <= min 
-        ? min 
-        : num >= max 
-            ? max 
-            : num
+function clamp(num : number, min : number, max : number) : number {
+    return ((num <= min) ? min : ((num >= max) ? max : num))
+}
+function clamp01(num : number) : number {
+    return ((num <= 0) ? 0 : ((num >= 1) ? 1 : num))
 }
 
 function handlescroll() {
-    
+    function calculate_opacity(bounds : DOMRect) : number {
+        return clamp((window.scrollY - bounds.top + transition_offset) / transition_smoothness, 0, 1)
+            -clamp((window.scrollY - bounds.bottom - bounds.top - transition_offset), 0, 1)
+    }
+    function calculate_transform(bounds : DOMRect) : number {
+        return (window.scrollY - bounds.top) / parallax_multiplier
+    }
 
     // About section
     {
-        var bounds : DOMRect = about_rect
-
-        var computed_opacity : number = clamp((window.scrollY - bounds.top + transition_offset) / transition_smoothness, 0, 1)
-            -clamp((window.scrollY - bounds.bottom - bounds.top - transition_offset), 0, 1)
-        var computed_offset : number = (window.scrollY - bounds.top) / parallax_multiplier
+        var computed_opacity : number = calculate_opacity(about_rect)
+        var computed_offset : number = calculate_transform(about_rect)
         
         about.style.opacity = computed_opacity.toString()
         about_content.style.transform = "translateY(" + -computed_offset + "px)"
@@ -60,15 +61,21 @@ function handlescroll() {
     
     // Repos section
     {
-        var bounds : DOMRect = repos_rect
-
-        var computed_opacity : number = clamp((window.scrollY - bounds.top + transition_offset) / transition_smoothness, 0, 1)
-            -clamp((window.scrollY - bounds.bottom - bounds.top - transition_offset), 0, 1)
-        var computed_offset : number = (window.scrollY - bounds.top) / parallax_multiplier
+        console.log({
+            Y : window.scrollY,
+            top : repos_rect.top,
+            height : repos_rect.height,
+            bottom : repos_rect.bottom,
+            offset : transition_offset
+        })
+        
+        var computed_opacity : number = calculate_opacity(repos_rect)
+        var computed_offset : number = calculate_transform(repos_rect)
         
         repos.style.opacity = computed_opacity.toString()
         repos_content.style.transform = "translateY(" + -computed_offset + "px)"
     }
+
 }
 
 function fetchpersonalrepos(root: HTMLElement) {
