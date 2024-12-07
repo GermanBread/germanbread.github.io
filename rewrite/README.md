838d38f6dd7054cd9a63963c6d898302fe5646ea
Emma <contact@emma.pizza>
2024-12-07 18:40:23 +0100

    https://youtu.be/oR0WtM_zE-M

---
+++ rewrite/README.md
@@ -0,0 +1,16 @@
+# germanbread.github.io
+
+*My personal homepage*
+
+---
+
+## Compiling
+
+### Without Nix
+
+* Install `sassc` and `tsc`
+* Run `make`
+
+### With Nix
+
+`nix-shell -p sassc nodePackages.typescript --run make`
--- rewrite/script.js
+++ rewrite/script.js
@@ -6,31 +6,87 @@ function init() {
     handlescroll();
     window.addEventListener("scroll", handlescroll);
     window.addEventListener("scroll", hidescrollhint);
+    fetchpersonalrepos(document.getElementById("repos-list"));
+    fetchcontibutionrepos(document.getElementById("contributions-list"));
 }
 function handlescroll() {
+    var elements = document.body.getElementsByTagName("window");
     if (window.scrollY > 50) {
         nav.classList.add("attached");
-    }
-    else {
-        nav.classList.remove("attached");
-    }
-}
-function hidescrollhint() {
-    if (window.scrollY > 50) {
-        hint.classList.add("hidden");
-        var elements = document.body.getElementsByTagName("window");
         for (var index = 1; index < elements.length; index++) {
             var element = elements[index];
             element.classList.remove("hidden");
         }
-        //window.removeEventListener("scroll", hidescrollhint)
     }
     else {
-        hint.classList.remove("hidden");
-        var elements = document.body.getElementsByTagName("window");
+        nav.classList.remove("attached");
         for (var index = 1; index < elements.length; index++) {
             var element = elements[index];
             element.classList.add("hidden");
         }
     }
+}
+function hidescrollhint() {
+    if (window.scrollY > 50) {
+        hint.classList.add("hidden");
+    }
+    else {
+        hint.classList.remove("hidden");
+    }
+}
+function fetchpersonalrepos(root) {
+    fetch('https://api.github.com/users/GermanBread/repos')
+        .then(function name(response) {
+        if (!response.ok) {
+            throw "Github responded with " + response.status;
+        }
+        response.json().then(function name(data) {
+            root.innerHTML = "";
+            for (var index = 0; index < data.length; index++) {
+                var element = data[index];
+                root.appendChild(createpanel(element));
+            }
+        });
+    })["catch"](function name(error) {
+        root.innerHTML = error;
+    });
+}
+function fetchcontibutionrepos(root) {
+    root.innerHTML = "";
+    fetch('https://api.github.com/repos/Lightcord/Lightcord')
+        .then(function name(response) {
+        if (!response.ok) {
+            throw "Github responded with " + response.status;
+        }
+        response.json().then(function name(data) {
+            root.appendChild(createpanel(data));
+        });
+    })["catch"](function name(error) {
+        root.innerHTML = error;
+    });
+    fetch('https://api.github.com/repos/arch-community/qbot')
+        .then(function name(response) {
+        if (!response.ok) {
+            throw "Github responded with " + response.status;
+        }
+        response.json().then(function name(data) {
+            root.appendChild(createpanel(data));
+        });
+    })["catch"](function name(error) {
+        root.innerHTML = error;
+    });
+}
+function createpanel(data) {
+    var _a;
+    var panel = document.createElement("a");
+    var header = document.createElement("h3");
+    var content = document.createElement("p");
+    panel.href = data.html_url;
+    panel.classList.add("list-panel");
+    header.innerHTML = data.name + (data.archived ? " (archived)" : "") + (data.fork ? " (forked)" : "");
+    content.innerHTML = (_a = data.description) !== null && _a !== void 0 ? _a : "No description provided";
+    panel.appendChild(header);
+    panel.appendChild(document.createElement("hr"));
+    panel.appendChild(content);
+    return panel;
 }
