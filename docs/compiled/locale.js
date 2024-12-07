efdfad41be21192593371242a11ca0db31d0fd94
Emma <contact@emma.pizza>
2024-12-07 18:41:11 +0100

    where commit

--- docs/compiled/locale.js
+++ docs/compiled/locale.js
@@ -1,7 +1,4 @@
 var locale;
 function initlocale() {
     locale = navigator.language.match("\\w+")[0];
-    // My plans:
-    // Language selection when you first visit this site or maybe not?
-    // Either way there should be a loading screen of sorts that hides the placeholder text
 }
